7934c030d0cde2473a49a58d2499717effa419a3
BroccoliSurprise <61516592+BroccoliSurprise@users.noreply.github.com>
2026-06-10 14:26:48 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    bitbaot.goms(BBDirection.Forward, 80, 2000)
-    bitbaot.rotatems(BBRobotDirection.Left, 60, 500)
-})
-bitbaot.setStarboardColor(0x00FFFF)
-bitbaot.setPortColor(0xFF00FF)
+
